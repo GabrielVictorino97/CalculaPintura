469c18b66784d90b11201b9816f081e883f9219f
Gabriel Victorino <gabriel.augusto.vict@hotmail.com>
2019-10-17 18:57:48 -0300

    Criação da Classe

---
+++ CalculadoraPintura.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculaPintura
+{
+    class CalculadoraPintura
+    {
+       public double areaParedes;
+       public double areaTeto;
+
+        public double CalcularAreaParede(double largura,double profundidade,double altura)
+        {
+           areaParedes = 2 * (largura + profundidade) * altura;
+            return areaParedes;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculaPintura
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CalculadoraPintura calculadora = new CalculadoraPintura();
+            
+            Console.WriteLine("Qual a largura ");
+            double largura = double.Parse(Console.ReadLine());
+            
+            Console.WriteLine("Qual a profundidade?");
+            double profundidade = double.Parse(Console.ReadLine());
+         
+            Console.WriteLine("A área das paredes é: ");
+            const double Altura = 2.9;
+
+            Console.WriteLine(calculadora.CalcularAreaParede(largura, profundidade, Altura));
+            Console.WriteLine("A área do teto é: ");
+
+            calculadora.areaTeto = largura * profundidade;
+            Console.WriteLine (calculadora.areaTeto);
+
+            Console.WriteLine("A litragem de tinta necessária é ");
+            Console.WriteLine((calculadora.areaParedes + calculadora.areaTeto) / 10);
+
+            Console.ReadKey();
+        }
+    }
+}
